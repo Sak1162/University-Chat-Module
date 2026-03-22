4c5217fe3a31c0212bf704969de594ecba566398
Sak1162 <partesakshi01@gmail.com>
2026-03-22 19:34:54 +0530

    bcrypt to bcryptjs

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const http = require("http");
 const { Server } = require("socket.io");
 
